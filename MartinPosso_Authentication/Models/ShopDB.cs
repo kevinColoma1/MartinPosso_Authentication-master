753900f9af4feafa67b2f9baccbaf6f47116af6c
Kevin Alexander Coloma Rodriguez <kevin.coloma@udla.edu.ec>
2020-12-31 18:24:39 -0500

    Agregar archivos de proyecto.

---
+++ MartinPosso_Authentication/Models/ShopDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MartinPosso_Authentication.Models
+{
+    public class ShopDB : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<User> Users { get; set; }
+                
+    }
+}
